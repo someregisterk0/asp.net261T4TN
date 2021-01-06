4ffd4c930a736ca446b95cadf53eb241d41f0854
danhnh <danhnh@email.com>
2021-01-06 21:14:49 +0700

    06-01-2021

---
+++ Module-03/slnWebAppBikeStore/WebApp/Controllers/DummyController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Controllers
+{
+    public class DummyController : Controller
+    {
+        IConfiguration configuration;
+
+        public DummyController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            string connectionString = configuration.GetConnectionString("BikeStore");
+            
+            return View("Index", connectionString);
+        }
+    }
+}
